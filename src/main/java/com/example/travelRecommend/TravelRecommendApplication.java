2f74f13b5f2508621be54707684b80228a6d20d6
SaiMeghana08 <meghanasai703@gmail.com>
2026-01-25 15:22:41 +0530

    Basic

---
+++ src/main/java/com/example/travelRecommend/TravelRecommendApplication.java
@@ -0,0 +1,13 @@
+package com.example.travelRecommend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelRecommendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelRecommendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/travelRecommend/TravelRecommendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.travelRecommend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelRecommendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
